6035cfcc8568e71a281b07347316c4bec72c332d
davidspringean12 <springeandavid5@gmail.com>
2025-05-13 10:08:33 +0200

    Initial Commit

---
+++ rtbvios/rtbviosApp.swift
@@ -0,0 +1,17 @@
+//
+//  rtbviosApp.swift
+//  rtbvios
+//
+//  Created by David Springean on 13.05.25.
+//
+
+import SwiftUI
+
+@main
+struct rtbviosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ rtbviosTests/rtbviosTests.swift
@@ -0,0 +1,17 @@
+//
+//  rtbviosTests.swift
+//  rtbviosTests
+//
+//  Created by David Springean on 13.05.25.
+//
+
+import Testing
+@testable import rtbvios
+
+struct rtbviosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
